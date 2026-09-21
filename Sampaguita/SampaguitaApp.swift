9d13a603b9e6926c637c3bdf4a48f475177e275c
Nick Istre <nistre.dev@gmail.com>
2026-09-21 00:10:56 -0400

    Initial Commit

---
+++ Sampaguita/SampaguitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampaguitaApp.swift
+//  Sampaguita
+//
+//  Created by Nick Istre on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct SampaguitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
